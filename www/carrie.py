7d3f8f38a52ecad9e38b3f7f9bb073535829ac6b
Hazel Victoria Campbell <github@hazel.zone>
2020-08-14 20:05:57 -0600

    marf?

--- www/carrie.py
+++ www/carrie.py
@@ -1,42 +1,96 @@
+#!/usr/bin/env python3
+
+# carrie.py - homepage for reverse proxy
+# Copyright &copy; 2020 Hazel Victoria Campbell
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import subprocess
+import sys
+import time
+
+import logging
+logger = logging.getLogger(__name__)
+DEBUG = logger.debug
+INFO = logger.info
+WARNING = logger.warning
+ERROR = logger.error
+CRITICAL = logger.critical
+
 from flask import Flask, render_template
 app = Flask(__name__, template_folder='.')
 
 class Status:
-    def get_cpu(self):
-        with open('/proc/stat') as fh:
-            lines = fh.readlines()
-        cpu = None
+    def getcol(self, lines, col):
+        val = -1.0
+        index = None
         for line in lines:
-            if 'cpu ' in line:
-                cpu = line
-        cpu = list(map(int, cpu.split()[1:]))
-        idle = (cpu[2] + cpu[4]) * 100 / sum(cpu[1:])
+            split = line.split()
+            if col in split:
+                index = split.index(col)
+            elif (
+                index is not None 
+                and index < len(split)
+                and 'Average' not in split[0]
+                ):
+                try:
+                    val = float(split[index].replace('%', ''))
+                except ValueError:
+                    pass
+        return val
+    
+    def du(self):
+        lines = subprocess.run(
+            [
+                '/bin/df',
+                '/var'
+                ],
+            capture_output=True,
+            ).stdout.decode().splitlines()
+        used = self.getcol(lines, 'Use%')
+        return f"{used:2.0f}"
+    
+    def sar(self, opt, col):
+        now = time.time()
+        then = now - 100
+        now = time.strftime('%H:%M:%S', time.localtime(now))
+        then = time.strftime('%H:%M:%S', time.localtime(then))
+        lines = subprocess.run(
+            [
+                '/usr/bin/sar',
+                '-0',
+                opt,
+                '-s', then,
+                '-e', now
+                ],
+            capture_output=True,
+            ).stdout.decode().splitlines()
+        return self.getcol(lines, col)
+    
+    def get_cpu(self):
+        idle = self.sar('-u', '%idle')
         used = 100 - idle
         return f"{used:2.0f}"
 
     def get_ram(self):
-        with open('/proc/meminfo') as fh:
-            lines = fh.readlines()
-        total = None
-        free = None
-        buff = None
-        cache = None
-        for line in lines:
-            if 'MemTotal' in line:
-                total = int(line.split()[1])
-            elif 'MemFree' in line:
-                free = int(line.split()[1])
-            elif 'Buffers' in line:
-                buff = int(line.split()[1])
-            elif 'Cached' in line:
-                cached = int(line.split()[1])
-        used = total - free - buff - cached
-        used = used * 100 / total
+        used = self.sar('-r', '%memused')
         return f"{used:2.0f}"
 
     def __init__(self):
         self.cpu = self.get_cpu()
         self.ram = self.get_ram()
+        self.disk = self.du()
 
 @app.route("/")
 def index():
--- www/carrie.wsgi
+++ www/carrie.wsgi
@@ -1,1 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www')
+
 from carrie import app as application
